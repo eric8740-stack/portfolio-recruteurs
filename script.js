677bdcc7ed8a737cc10018ca991ce36d166ef123
Eric Paysant <eric.paysant@outlook.fr>
2026-04-10 08:54:57 +0200

    Fix URL rating v2

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ const counterObserver = new IntersectionObserver((entries) => {
 statNumbers.forEach(num => counterObserver.observe(num));
 
 // RATING
-const RATING_API = 'https://script.google.com/macros/s/AKfycbxIlurw2aotR-KQ3a9v1mHsS7XrfK-B6h7Y_4-8oKzvKCTXN-vB_ocxIt8fZpNLX18/exec';
+const RATING_API = 'https://script.google.com/macros/s/AKfycbzpOta4QQ1rhijm5v1zt6hMv5NVhbzM1iLrgZlVK4ay2iMyheYQ0lSNbAbW44OHfU0/exec';
 
 function loadRating() {
     fetch(RATING_API).then(r => r.json()).then(data => {
